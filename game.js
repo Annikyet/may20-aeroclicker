403beb2f07c3ddc4364696c6b244d195c23c442e
Annika <annikyet@icloud.com>
2022-05-20 13:19:06 -0600

    hopefully ignore photos

--- game.js
+++ game.js
@@ -46,67 +46,67 @@ class Plane {
 let planes = {
   c150: new Plane(
     'The Tin Can',
-    'c150.jpg',
+    'img/c150.jpg',
     10,
     1
   ),
   bonanza: new Plane(
     'The Doctor Killer',
-    'bonanza.jpg',
+    'img/bonanza.jpg',
     100,
     5
   ),
   dc3: new Plane(
     'The Classic',
-    'dc3.jpg',
+    'img/dc3.jpg',
     1000,
     200
   ),
   comet: new Plane(
     'The Depressurizer',
-    'comet.jpg',
+    'img/comet.jpg',
     10000,
     1000
   ),
   q400: new Plane(
     'The Puddlejumper',
-    'q400.jpg',
+    'img/q400.jpg',
     100000,
     5000
   ),
   a220: new Plane(
     'The Hostile Takeover',
-    'a220.jpg',
+    'img/a220.jpg',
     250000,
     20000
   ),
   max: new Plane(
     'The Nosedown',
-    '737max.jpg',
+    'img/737max.jpg',
     2000000,
     100000
   ),
   dreamliner: new Plane(
     'The Sleepy Boi',
-    '787.jpg',
+    'img/787.jpg',
     10000000,
     500000
   ),
   queen: new Plane(
     'The Queen',
-    '747.jpg',
+    'img/747.jpg',
     50000000,
     2000000
   ),
   mriya: new Plane(
     'Big Chungus',
-    'mriya.jpg',
+    'img/mriya.jpg',
     500000000,
     10000000
   ),
   concorde: new Plane(
     'The Leadfoot',
-    'concorde.jpg',
+    'img/concorde.jpg',
     2000000000,
     50000000
   )
